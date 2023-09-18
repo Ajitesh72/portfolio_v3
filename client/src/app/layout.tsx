f680a0fa5713c4b71c6dcad1eea578f02385efdb
Ajitesh <Ajitesh72>
2023-09-18 23:48:21 +0530

    aos

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,9 +1,9 @@
-"use client"
+// "use client"
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+// import AOS from 'aos';
+// import 'aos/dist/aos.css';
 import { useEffect } from 'react';
 const inter = Inter({ subsets: ['latin'] });
 
@@ -17,14 +17,14 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
-  useEffect(() => {
-    // Initialize AOS
-    AOS.init({
-      // duration: 000, // Animation duration
-      easing: 'ease-in-out', // Animation easing
-      once: false, // Animation only happens once per element
-    });
-  }, []); // Empty dependency array to ensure it runs only once
+  // useEffect(() => {
+  //   // Initialize AOS
+  //   AOS.init({
+  //     // duration: 000, // Animation duration
+  //     easing: 'ease-in-out', // Animation easing
+  //     once: false, // Animation only happens once per element
+  //   });
+  // }, []); // Empty dependency array to ensure it runs only once
 
   return (
     <html lang="en">
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,10 +1,22 @@
+"use client"
 import Landing from "./landing";
 import About from "./pages/aboutme";
 // import WebDev from "@/app/pages/webdev"
 import Footer from "./pages/footer";
 import Development from "./pages/development";
+import { useEffect } from "react";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 export default function Home() {
+    useEffect(() => {
+    // Initialize AOS
+    AOS.init({
+      // duration: 000, // Animation duration
+      easing: 'ease-in-out', // Animation easing
+      once: false, // Animation only happens once per element
+    });
+  }, []); // Empty dependency array to ensure it runs only once
   return (
     <main>
       <div id="home">
