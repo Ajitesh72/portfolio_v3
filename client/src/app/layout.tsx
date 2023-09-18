f4830b21048925054d70f41e60b9dd7b74a973ee
Ajitesh <Ajitesh72>
2023-09-18 23:49:29 +0530

    aos added

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,10 +1,6 @@
-// "use client"
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-// import AOS from 'aos';
-// import 'aos/dist/aos.css';
-import { useEffect } from 'react';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
@@ -17,15 +13,6 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
-  // useEffect(() => {
-  //   // Initialize AOS
-  //   AOS.init({
-  //     // duration: 000, // Animation duration
-  //     easing: 'ease-in-out', // Animation easing
-  //     once: false, // Animation only happens once per element
-  //   });
-  // }, []); // Empty dependency array to ensure it runs only once
-
   return (
     <html lang="en">
       <body className={inter.className}>
