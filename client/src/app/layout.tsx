759e614a8c73ab8aa5fa90f78f63e699b53cd8c7
Ajitesh <Ajitesh72>
2023-12-07 23:55:16 +0530

    meta tag added

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import type { Metadata } from "next";
+import Head from 'next/head';
 import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,7 +16,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-
+      <Head>
+        <meta name="google-site-verification" content="bLYa0V9I9aMRCBg6nWCVAHLx679j9iT0YJ0gVU63Lo0" />
+      </Head>
       <body className={inter.className}>{children}</body>
       
     </html>
